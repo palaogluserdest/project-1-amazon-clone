9527f40a51e5a26efb9cb9933fe7dd2f4b1935ce
palaogluserdest-dev <palaogluserdest@hotmail.com>
2024-10-21 22:13:59 +0300

    Added reset css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.module.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { StateContextProvider } from './provider/StateProvider.tsx';
+import './assets/styles/reset.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
